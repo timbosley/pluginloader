b15e57786d026a57d9318ba6100b5fb0ae6b3f5e
Tim Bosley <ursending@gmail.com>
2022-12-20 20:27:59 -0700

    Changes to loader

--- loader.java
+++ loader.java
@@ -1,21 +1,32 @@
 import java.io.*;
 import java.lang.reflect.*;
+import java.util.*;
 
 
 public class loader {
-
+final private static String wdir = System.getProperty("user.dir");
 
 	public static void main(String[] arg) {
-
-	load(".");
+		
+		try {
+			load();
+		}
+		catch (IOException I) {
+			System.out.println(I.toString());
+		}
 	}
 
-	public static void load() {
-	
-	load(/*whatever is the working directory variable*/);
+	public static void load() throws IOException {
+
+		load(wdir, Comparator.<String>naturalOrder());
 	}
 
-	public static void load(String dir) {
+	public static void loadFrom(String subdir, Comparator ord) throws IOException {
+		String dir = wdir + File.separator + subdir;
+		load(subdir, Comparator.<String>naturalOrder());
+	}
+
+	public static void load(String dir, Comparator ord) throws IOException {
 		class KlassFilter implements FilenameFilter {
 
 			public boolean accept(File d, String f) {
@@ -32,36 +43,37 @@ public class loader {
 			}
 
 		}
-		try {
-			File fdir = new File(dir);
-			File[] patches = fdir.listFiles(new KlassFilter());
-			for (File patch : patches) {
-				try {
-					String filename = patch.getName();
-					String[] klassname = filename.split("[.]", 2);
+		File fdir = new File(dir);
+		if (fdir.isDirectory() == false) throw new IOException(fdir.toString() + " is not a directory!");
+		String[] files = fdir.list(new KlassFilter());
+		Arrays.sort(files, ord);
+		for (String filename : files) {
+			try {
+				String[] klassname = filename.split("[.]", 2);
 
-					Class<?> klass = ClassLoader.getSystemClassLoader().loadClass(klassname[0]);
-						try {
-							Runnable rable = (Runnable)klass.newInstance();
-							if (rable instanceof Runnable) {
-							rable.run();
-							}
+				Class<?> klass = ClassLoader.getSystemClassLoader().loadClass(klassname[0]);
+					try {
+						Runnable rable = (Runnable)klass.newInstance();
+						if (rable instanceof Runnable) {
+						rable.run();
 						}
-						catch (ClassCastException C) {
-							System.out.println(klassname[0]);
-							System.out.println(C.toString());
-						}
+					}
+					catch (InstantiationException I) {
+						System.out.println(I.toString());
+					}
+					catch (IllegalAccessException I) {
+						System.out.println(I.toString());
+					}
+					catch (ClassCastException C) {
+						System.out.println(klassname[0]);
+						System.out.println(C.toString());
+					}
 
 
-				}
-				catch (ClassNotFoundException C) {
-					System.out.println(C.toString());
-				}
 			}
-		}
-		catch (Exception I) {
-			System.out.println(I.toString());
-		
+			catch (ClassNotFoundException C) {
+				System.out.println(C.toString());
+			}
 		}
 	}
 
